fc2de2bdf1d24b6bf0704d32dbe5fee40de4371b
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-05-21 23:09:17 -0400

    App wrapped with withrouter

--- water-my-plant/src/index.js
+++ water-my-plant/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, withRouter} from 'react-router-dom';
 
-
+const AppwithRouter = withRouter(App)
 ReactDOM.render(
   <Router>
-    <App />
+    <AppwithRouter />
   </Router>  , document.getElementById('root'));
